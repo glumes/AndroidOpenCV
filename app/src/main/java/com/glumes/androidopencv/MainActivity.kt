00380579d73aaba068f14efc8194803721540805
glumes <zhaoying9402@gmail.com>
2017-11-17 00:45:36 +0800

    config android java develop environment

---
+++ app/src/main/java/com/glumes/androidopencv/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.glumes.androidopencv
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_main.*
+import org.opencv.android.*
+import org.opencv.core.Mat
+
+class MainActivity : AppCompatActivity(), CameraBridgeViewBase.CvCameraViewListener2 {
+
+    override fun onCameraViewStarted(width: Int, height: Int) {
+
+    }
+
+    override fun onCameraViewStopped() {
+
+    }
+
+    override fun onCameraFrame(inputFrame: CameraBridgeViewBase.CvCameraViewFrame?): Mat {
+        return inputFrame!!.rgba()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main)
+        camera_view.setCvCameraViewListener(this)
+
+    }
+
+    var baseLoaderCallback: BaseLoaderCallback = object : BaseLoaderCallback(this) {
+        override fun onManagerConnected(status: Int) {
+            when (status) {
+                LoaderCallbackInterface.SUCCESS -> {
+                    camera_view.enableView()
+                }
+                else -> {
+                    super.onManagerConnected(status)
+                }
+            }
+
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        if (!OpenCVLoader.initDebug()) {
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_3_0, this, baseLoaderCallback);
+        } else {
+            baseLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (camera_view != null)
+            camera_view.disableView()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
